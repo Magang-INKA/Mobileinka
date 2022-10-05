9977f8967d7c3b6d7c1112601f4bab2505e2e7ff
vegaangga <vegaangga27@gmail.com>
2022-10-05 16:31:44 +0700

    add settings page

---
+++ lib/pages/change_password.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutterinka/util/emoji_card.dart';
+import 'package:flutterinka/widgets/navbar.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PagePassword extends StatefulWidget {
+  const PagePassword({super.key});
+
+  @override
+  State<PagePassword> createState() => _PagePasswordState();
+}
+
+class _PagePasswordState extends State<PagePassword> {
+
+  int _selectedItemIndex = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.blue[800],
+      // bottomNavigationBar: BottomNavigation(),
+      body: Container(
+        height: 1000,
+        decoration: BoxDecoration(color: Colors.blue),
+        child: SafeArea(
+          child: Column(children: [
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                IconButton(
+                icon : const Icon(Icons.arrow_back,size: 30,color: Colors.white,), onPressed: () {
+                  Navigator.pop(context);
+                 },
+                ),
+              ],
+            ),
+            // Profiles row
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Column(children: [
+                 Center(
+                   child: Column(
+                     children: [
+                      Row(children: [
+                        Text('Ubah Kata Sandi',
+                        style: TextStyle(color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                        ),
+                      ],),
+                      const Divider(
+                        thickness: 2,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                      TextField(
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Password Lama",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Password Baru",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+
+                          SizedBox(
+                            height: 15,
+                          ),
+                     Container(
+                       padding: EdgeInsets.all(15),
+                       decoration: BoxDecoration(
+                         color: Colors.white,
+                         borderRadius: BorderRadius.circular(15)
+                         ),
+                         child:
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                           children: [
+                             Text(
+                               'Simpan',
+                               style: TextStyle(
+                                 fontWeight: FontWeight.bold,
+                               ),
+                               ),
+                           ],
+                         ),
+                      ),
+
+                     ],
+                   ),
+                 ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
